6e4b9f8a19495575537f34b2582c65e2adb84d87
Omegashin <gdesigner.arshdeep@gmail.com>
2021-03-09 17:28:07 -0500

    feat: added flask api connection

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const black = '#fff';
+const black = '#aaa';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,9 +14,21 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(() => {
+    fetch('http://localhost:5000/time')
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+        console.log(data);
+      })
+      .catch((error) => console.log(error.message));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app.</Text>
+      <Text>The current time is {currentTime}.</Text>
       <StatusBar />
     </View>
   );
