a9ce1e294f03713e65646971b99ddbe4f1dcac66
Omegashin <gdesigner.arshdeep@gmail.com>
2021-03-09 23:20:06 -0500

    style: added global COLORS and styles

---
+++ colors.js
@@ -0,0 +1,3 @@
+export default {
+  loadingBackground: '#e3f2fd',
+};
---
+++ styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+// global colors
+const red = 'green';
+
+export default StyleSheet.create({
+  alwaysred: {
+    backgroundColor: red,
+    height: 100,
+    width: 100,
+  },
+});
