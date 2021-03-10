518ec815ff2c375e087ea9ed60e00160f0416194
Omegashin <gdesigner.arshdeep@gmail.com>
2021-03-09 22:10:32 -0500

    style: split jpx sections; added custom fonts

--- App.jsx
+++ App.jsx
@@ -4,53 +4,104 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
+import AppLoading from 'expo-app-loading';
+/* eslint-disable camelcase */
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_700Bold,
+  Inter_500Medium,
+  Inter_400Regular,
+  Inter_300Light,
+} from '@expo-google-fonts/inter';
+/* eslint-enable camelcase */
 
-const black = '#aaa';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: black,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*
+---------
+Behaviour
+---------
+  */
 
 export default function App() {
   // component states
+  const [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_700Bold,
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_300Light,
+  });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState({});
   const [currentWeather, setCurrentWeather] = useState(0);
-
-  const fetchWeather = async (lat, lon) => {
+  // connect to api and fetch the current weather
+  const fetchWeather = async (coords) => {
     await axios
-      .get(`http://localhost:5000/current?lat=${lat}&lon=${lon}`)
+      .get(
+        `http://localhost:5000/current?lat=${coords.latitude}&lon=${coords.longitude}`
+      )
       .then((res) => {
+        // finished loading
+        console.log(res.data.list[0].main.temp);
+        // TODO handle null list
+        setCurrentWeather(res.data.list[0]);
         setLoading(false);
-        setCurrentWeather(res.data.list[0].main.temp);
       })
       .catch((e) => {
+        // set error state
         setLoading(false);
         setError(e);
       });
   };
-
+  // capture user's current location
   const getLocationAsync = async () => {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status === 'granted') {
+      // user granted location permission
       const res = await Location.getCurrentPositionAsync({
         enableHighAccuracy: true,
       });
-      fetchWeather(res.coords.latitude, res.coords.longitude);
+      fetchWeather(res.coords);
+      // user refused location permission
     } else {
-      throw new Error('Location permission not granted');
+      setError({ message: 'Location permission not granted!' });
+      throw new Error('Location permission not granted!');
     }
   };
-
+  // onComponentMount
   useEffect(() => {
     getLocationAsync();
-  }, [loading]);
+  }, [loading, error]);
 
+  /*
+------
+Styles
+------
+  */
+
+  const black = '#aaa';
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: black,
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'Inter_300Light',
+    },
+  });
+
+  /*
+--------
+Template
+--------
+  */
+
+  // load fonts
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  // start app
   if (loading) {
     return (
       <View style={styles.container}>
@@ -58,11 +109,12 @@ export default function App() {
       </View>
     );
   }
+  // show weather components
   return (
     <View style={styles.container}>
       <Text>Loading is {loading}.</Text>
       <Text>Error is {error.message}.</Text>
-      <Text>The current time is {currentWeather}.</Text>
+      <Text>The current time is {currentWeather.main.temp}.</Text>
       <StatusBar />
     </View>
   );
