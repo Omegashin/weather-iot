302e84cd33db000ef44a55de48cd91af2d895d8b
Omegashin <gdesigner.arshdeep@gmail.com>
2021-03-09 20:19:49 -0500

    feat: added cors, state app lifecycle scafolding

--- App.jsx
+++ App.jsx
@@ -1,6 +1,9 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
 const black = '#aaa';
 
@@ -14,21 +17,52 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  useEffect(() => {
-    fetch('http://localhost:5000/time')
-      .then((res) => res.json())
-      .then((data) => {
-        setCurrentTime(data.time);
-        console.log(data);
-      })
-      .catch((error) => console.log(error.message));
-  }, []);
+  // component states
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({});
+  const [currentWeather, setCurrentWeather] = useState(0);
 
+  const fetchWeather = async (lat, lon) => {
+    await axios
+      .get(`http://localhost:5000/current?lat=${lat}&lon=${lon}`)
+      .then((res) => {
+        setLoading(false);
+        setCurrentWeather(res.data.list[0].main.temp);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(e);
+      });
+  };
+
+  const getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === 'granted') {
+      const res = await Location.getCurrentPositionAsync({
+        enableHighAccuracy: true,
+      });
+      fetchWeather(res.coords.latitude, res.coords.longitude);
+    } else {
+      throw new Error('Location permission not granted');
+    }
+  };
+
+  useEffect(() => {
+    getLocationAsync();
+  }, [loading]);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app.</Text>
-      <Text>The current time is {currentTime}.</Text>
+      <Text>Loading is {loading}.</Text>
+      <Text>Error is {error.message}.</Text>
+      <Text>The current time is {currentWeather}.</Text>
       <StatusBar />
     </View>
   );
--- api/api.py
+++ api/api.py
@@ -18,25 +18,28 @@ def get_current_time():
 
 
 @app.route("/current")
-def current_by_city():
+def current_by_coords():
 
-    # passed city argument
-    city = request.args.get("city")
+    # get passed arguments
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
 
     # get response as a python dict
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={app.config.get('OPEN_WEATHER_MAP_API_KEY')}"
+    url = f"https://api.openweathermap.org/data/2.5/find?lat={lat}&lon={lon}&cnt=1&units=metric&appid={app.config.get('OPEN_WEATHER_MAP_API_KEY')}"
     response = requests.get(url).json()
 
     # basic error handling
-    if response.get("cod") != 200:
+    if response.get("cod") != "200":
         message = response.get("message", "")
         return f"Error = {message}"
 
     # get current weather
     # TODO extract all fields
-    current_temperature = response.get("main", {}).get("temp")
-    if current_temperature:
-        current_temperature_celsius = round(current_temperature - 273.15, 2)
-        return f"Current temperature of {city.title()} is {current_temperature_celsius} &#8451;"
-    else:
-        return f"Error getting temperature for {city.title()}"
+
+    return response
+    # current_temperature = response.get("main", {}).get("temp")
+    # if current_temperature:
+    #     current_temperature_celsius = round(current_temperature - 273.15, 2)
+    #     return f"Current temperature of {city.title()} is {current_temperature_celsius} &#8451;"
+    # else:
+    #     return f"Error getting temperature for {city.title()}"
